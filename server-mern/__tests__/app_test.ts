89f96bb8c0cf788152cdb01bbd5ec58a29486706
Historia Montague <historia.montague@outlook.com>
2023-09-05 12:43:42 -0600

    added unit tests for other api routes

--- server-mern/__tests__/app_test.ts
+++ server-mern/__tests__/app_test.ts
@@ -1,32 +1,46 @@
 const request = require("supertest");
-const app = require("../app")
+const app = require("../app");
 
-// describe('Test the addLike method', () => {
-//   beforeAll(() => {
-//       mongoDB.connect();
-//   });
-
-//   afterAll((done) => {
-//       mongoDB.disconnect(done);
-//   });
-// }
+const id = 1;
 
 describe("Test for the root path", () => {
   test("Should respond to a GET with a simple h1", async () => {
     const resp = await request(app).get("/");
     expect(resp.statusCode).toBe(200);
-    expect(resp.text).toContain("<h1>Zoo app</h1>")
+    expect(resp.text).toContain("<h1>Zoo app</h1>");
   });
 });
 
 describe("Has a resource at '/animal/:id'", () => {
-  test("Responds to a get with a li containing animal information", async () => {
-    const resp = await request(app).get(`/animal/${1}`)
+  test("Responds to a GET with a li containing animal information", async () => {
+    const resp = await request(app).get(`/animal/${id}`);
     expect(resp.statusCode).toBe(200);
-    expect(resp.text).toMatch(/\<li\>+/)
-    expect(resp.text).toMatch(/\<ul\>[Nn]ame.+/)
-    expect(resp.text).toMatch(/\<ul\>[Ss]pecies.+/)
-    expect(resp.text).toMatch(/\<ul\>[Zz]ookeeper.+/)
-    expect(resp.text).toMatch(/\<ul\>[Ee]nclosure.+/)
-  })
+    expect(resp.text).toMatch(/\<li\>+/);
+    expect(resp.text).toMatch(/\<ul\>[Nn]ame.+/);
+    expect(resp.text).toMatch(/\<ul\>[Ss]pecies.+/);
+    expect(resp.text).toMatch(/\<ul\>[Zz]ookeeper.+/);
+    expect(resp.text).toMatch(/\<ul\>[Ee]nclosure.+/);
+  });
+});
+
+describe("Has a resource at '/zookeeper/:id'", () => {
+  test("Responds to a GET with a li containing zookeeper information", async () => {
+    const resp = await request(app).get(`/zookeeper/${id}`);
+    expect(resp.statusCode).toBe(200);
+    expect(resp.text).toMatch(/\<li\>+/);
+    expect(resp.text).toMatch(/\<ul\>[Nn]ame.+/);
+    expect(resp.text).toMatch(/\<ul\>[Aa]nimal.+/);
+    expect(resp.text).toMatch(/\<ul\>[Bb]irthday.+/);
+  });
+});
+
+describe("Has a resource at '/enclosure/:id'", () => {
+  test("Responds to a GET with a li containing enclosure information", async () => {
+    const resp = await request(app).get(`/enclosure/${id}`);
+    expect(resp.statusCode).toBe(200);
+    expect(resp.text).toMatch(/\<li\>+/);
+    expect(resp.text).toMatch(/\<ul\>[Ee]nvironment.+/);
+    expect(resp.text).toMatch(/\<ul\>[Oo]pen\s[Tt]o\s[Vv]isitors.+/);
+    expect(resp.text).toMatch(/\<ul\>[Aa]nimal.+/);
+  });
 });
